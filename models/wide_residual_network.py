c9068c7879d15e45e60db5fc3cdbbd38a45a59f2
yaldashbz <yaldashabanzadeh@gmail.com>
2022-03-13 15:20:05 +0330

    fix: keras version conflict in legacy

--- models/wide_residual_network.py
+++ models/wide_residual_network.py
@@ -1,10 +1,11 @@
+import numpy as np
+
 from keras.models import Model
 from keras.layers import *
-from keras.regularizers import l2
-from keras.initializers import _compute_fans
-from keras.optimizers import SGD
 from keras import backend as K
-
+from keras.regularizers import l2
+from models import interfaces
+from tensorflow.keras.optimizers import SGD
 
 WEIGHT_DECAY = 0.5 * 0.0005
 
@@ -38,6 +39,43 @@ class SGDTorch(SGD):
 
             self.updates.append(K.update(p, new_p))
         return self.updates
+
+
+def _compute_fans(shape, data_format='channels_last'):
+    """Computes the number of input and output units for a weight shape.
+    # Arguments
+        shape: Integer shape tuple.
+        data_format: Image data format to use for convolution kernels.
+            Note that all kernels in Keras are standardized on the
+            `channels_last` ordering (even when inputs are set
+            to `channels_first`).
+    # Returns
+        A tuple of scalars, `(fan_in, fan_out)`.
+    # Raises
+        ValueError: in case of invalid `data_format` argument.
+    """
+    if len(shape) == 2:
+        fan_in = shape[0]
+        fan_out = shape[1]
+    elif len(shape) in {3, 4, 5}:
+        # Assuming convolution kernels (1D, 2D or 3D).
+        # TH kernel shape: (depth, input_depth, ...)
+        # TF kernel shape: (..., input_depth, depth)
+        if data_format == 'channels_first':
+            receptive_field_size = np.prod(shape[2:])
+            fan_in = shape[1] * receptive_field_size
+            fan_out = shape[0] * receptive_field_size
+        elif data_format == 'channels_last':
+            receptive_field_size = np.prod(shape[:-2])
+            fan_in = shape[-2] * receptive_field_size
+            fan_out = shape[-1] * receptive_field_size
+        else:
+            raise ValueError('Invalid data_format: ' + data_format)
+    else:
+        # No specific assumptions.
+        fan_in = np.sqrt(np.prod(shape))
+        fan_out = np.sqrt(np.prod(shape))
+    return fan_in, fan_out
 
 
 def _get_channels_axis():
@@ -95,7 +133,7 @@ def _add_conv_group(x_in, out_channels, n, strides, dropout_rate=0.0):
 
 def create_wide_residual_network(input_shape, num_classes, depth, widen_factor=1, dropout_rate=0.0,
                                  final_activation='softmax'):
-    n_channels = [16, 16*widen_factor, 32*widen_factor, 64*widen_factor]
+    n_channels = [16, 16 * widen_factor, 32 * widen_factor, 64 * widen_factor]
     assert ((depth - 4) % 6 == 0), 'depth should be 6n+4'
     n = (depth - 4) // 6
 
